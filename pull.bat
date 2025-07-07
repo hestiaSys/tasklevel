4fa7c3ddd35e6c2670ed77235dd46adbaf2bd542
locatecima@gmail.com <locatecima@gmail.com>
2025-07-07 18:39:01 +0900

    20250707

--- pull.bat
+++ pull.bat
@@ -1,5 +1,1 @@
-@echo off
-set /p COMMENT="ƒRƒƒ“ƒg‚ğ“ü—Í‚µ‚Ä‚­‚¾‚³‚¢: "
-git add .
-git commit -m %COMMENT%
-git push
+git pull
---
+++ push.bat
@@ -0,0 +1,5 @@
+@echo off
+set /p COMMENT="ƒRƒƒ“ƒg‚ğ“ü—Í‚µ‚Ä‚­‚¾‚³‚¢: "
+git add .
+git commit -m %COMMENT%
+git push
